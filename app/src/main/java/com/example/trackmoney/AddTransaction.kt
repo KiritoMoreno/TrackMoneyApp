7d4158d8ef417584f6ed29db6dfdfa4e30247cca
kiritomoreno <moreno.algorithmictrading@gmail.com>
2024-02-27 21:25:37 +0000

    ( show error message or add new transaction)

--- app/src/main/java/com/example/trackmoney/AddTransaction.kt
+++ app/src/main/java/com/example/trackmoney/AddTransaction.kt
@@ -2,10 +2,50 @@ package com.example.trackmoney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class AddTransaction : AppCompatActivity() {
+    //When the add transaction button is clicked it will check if the fields are empty or not ( show error message or add new transaction)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
+
+        val labelInput: TextInputEditText = findViewById(R.id.labelInput)
+        val amountInput: TextInputEditText = findViewById(R.id.amountInput)
+        val labelLayout: TextInputLayout = findViewById(R.id.labelLayout)
+        val amountLayout: TextInputLayout = findViewById(R.id.amountLayout)
+
+        labelInput.addTextChangedListener{
+            if (it?.length ?: 0 > 0){
+                labelLayout.error= null
+            }
+        }
+
+        amountInput.addTextChangedListener{
+            if (it?.length ?: 0 > 0){
+                amountLayout.error= null
+            }
+        }
+
+        val addTransactionBtn: Button = findViewById(R.id.addTransactionBtn)
+        addTransactionBtn.setOnClickListener{
+            val labelInput: TextInputEditText = findViewById(R.id.labelInput)
+            val amountInput: TextInputEditText = findViewById(R.id.amountInput)
+            val labelLayout: TextInputLayout = findViewById(R.id.labelLayout)
+            val amountLayout: TextInputLayout = findViewById(R.id.amountLayout)
+
+            val label: String = labelInput.text.toString()
+            val amount: Double? = amountInput.text.toString().toDoubleOrNull()
+
+            if (label.isEmpty())
+                labelLayout.error = "Please enter a valid label"
+
+            if (amount == null)
+                amountLayout.error = "Please enter a valid amount"
+        }
     }
 }
