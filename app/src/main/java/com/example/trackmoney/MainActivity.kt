f627d2d7caa28afe27ea8c65336d8cf125d8420a
kiritomoreno <moreno.algorithmictrading@gmail.com>
2024-02-27 19:24:48 +0000

    Update balance budget and expense

--- app/src/main/java/com/example/trackmoney/MainActivity.kt
+++ app/src/main/java/com/example/trackmoney/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.trackmoney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -39,5 +40,23 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = transactionAdapter
 
+        updateDashboard()
+
     }
+    //
+    private fun updateDashboard(){
+        val totalAmount= transaction.map{it.amount}.sum() //New list with only the amounts of transaction
+        val budgetAmount= transaction.filter { it.amount>0 }.map { it.amount }.sum()  //This is the list of all the amounts of the transactions than are greater than zero
+        val expenseAmount= totalAmount - budgetAmount
+
+        val balance: TextView = findViewById(R.id.balance)
+        val budget: TextView = findViewById(R.id.budget)
+        val expense: TextView = findViewById(R.id.expense)
+
+        balance.text = "$ %.2f".format(totalAmount)
+        budget.text= "$ %.2f".format(budgetAmount)
+        expense.text="$ %.2f".format(expenseAmount)
+
+    }
+
 }
